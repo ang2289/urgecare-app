37b94a30f756a046d2831af3e737dcc114863c43
ang2289 <ang2289@gmail.com>
2025-09-04 20:12:28 +0800

    feat: 更新首頁樣式為活潑 APP 風格，改善排版與視覺層次

---
+++ android/app/src/main/java/com/urgecare/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.urgecare.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+const config = {
+  appId: 'com.urgecare.app',
+  appName: 'UrgeCare',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
